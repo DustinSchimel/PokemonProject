fce10f60f2094a6fcd5a6b849c593a74504b4569
Dustin Schimel <dustin.schimel@gmail.com>
2017-12-12 13:00:16 -0700

    Completed the runner

--- src/pokemon/controller/PokemonRunner.java
+++ src/pokemon/controller/PokemonRunner.java
@@ -1,6 +1,15 @@
 package pokemon.controller;
 
+/**
+ * Runs the project by calling the PokemonController
+ * @author Dustin Schimel
+ *
+ */
 public class PokemonRunner
 {
-
+	public static void main (String [] args)
+	{
+		PokemonController app = new PokemonController();
+		app.start();
+	}
 }
