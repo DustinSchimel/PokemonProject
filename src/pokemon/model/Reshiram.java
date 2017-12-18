0e81089a65b7df97dc006474d1cd407171afce15
Dustin Schimel <dustin.schimel@gmail.com>
2017-12-18 13:30:20 -0700

    Fixed Reshiram typing

--- src/pokemon/model/Reshiram.java
+++ src/pokemon/model/Reshiram.java
@@ -1,10 +1,25 @@
 package pokemon.model;
 
-public class Reshiram extends Pokemon implements Fire
+public class Reshiram extends Pokemon implements Dragon, Fire
 {
 	public Reshiram(int number, String name)
 	{
 		super(number, name);
+	}
+	
+	public String moveOutrage()
+	{
+		return this.getClass().getSimpleName() + " uses Outrage";
+	}
+	
+	public String moveDragonDance()
+	{
+		return this.getClass().getSimpleName() + " uses Dragon Dance";
+	}
+	
+	public String moveDragonPulse()
+	{
+		return this.getClass().getSimpleName() + " uses Dragon Pulse";
 	}
 	
 	public String moveFlamethrower()
