66c9c6aa8cdf0ec5f85159e236e5e38387df9a0f
Dustin Schimel <dustin.schimel@gmail.com>
2018-01-03 11:35:50 -0700

    Completed Groudon

--- src/pokemon/model/Groudon.java
+++ src/pokemon/model/Groudon.java
@@ -2,11 +2,32 @@ package pokemon.model;
 
 public class Groudon extends Pokemon implements Ground
 {
+	public Groudon()
+	{
+		super(383, "Groudon");
+		setup();
+	}
+	
+	public Groudon(String name)
+	{
+		super(383, name);
+		setup();
+	}
+	
 	public Groudon(int number, String name)
 	{
 		super(number, name);
 	}
 	
+	protected void setup()
+	{
+		this.setAttackPoints(150);
+		this.setCanEvolve(false);
+		this.setEnhancementModifier(1.5);
+		this.setHealthPoints(300);
+	}
+	
+	//Ground methods
 	public String moveEarthquake()
 	{
 		return this.getClass().getSimpleName() + " uses Earthquake";
