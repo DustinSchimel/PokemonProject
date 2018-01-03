190786656593305743bd7a1b8eaf7383a9c1ca28
Dustin Schimel <dustin.schimel@gmail.com>
2018-01-03 11:50:40 -0700

    Completed Darkrai

--- src/pokemon/model/Darkrai.java
+++ src/pokemon/model/Darkrai.java
@@ -2,11 +2,32 @@ package pokemon.model;
 
 public class Darkrai extends Pokemon implements Dark
 {
+	public Darkrai()
+	{
+		super(491, "Darkrai");
+		setup();
+	}
+	
+	public Darkrai(String name)
+	{
+		super(491, name);
+		setup();
+	}
+	
 	public Darkrai(int number, String name)
 	{
 		super(number, name);
 	}
 	
+	protected void setup()
+	{
+		this.setAttackPoints(150);
+		this.setCanEvolve(false);
+		this.setEnhancementModifier(1.5);
+		this.setHealthPoints(300);
+	}
+	
+	//Dark moves
 	public String moveDarkPulse()
 	{
 		return this.getClass().getSimpleName() + " uses Dark Pulse";
