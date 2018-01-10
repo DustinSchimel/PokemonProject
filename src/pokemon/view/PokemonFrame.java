147710a71766825cb93a1159d5e7d24377f80ceb
Dustin Schimel <dustin.schimel@gmail.com>
2018-01-10 12:05:46 -0700

    Created the pokemon frame

--- src/pokemon/view/PokemonFrame.java
+++ src/pokemon/view/PokemonFrame.java
@@ -1,6 +1,33 @@
 package pokemon.view;
 
-public class PokemonFrame
-{
+import pokemon.controller.PokemonController;
+import javax.swing.JFrame;
 
+public class PokemonFrame extends JFrame
+{
+	private PokemonController appController;
+	private PokemonPanel appPanel;
+	
+	public PokemonFrame(PokemonController appController)
+	{
+		super();
+		this.appController = appController;
+		appPanel = new PokemonPanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setTitle("Pokedex");
+		this.setResizable(false);
+		this.setSize(500, 500);
+		this.setVisible(true);
+	}
+	
+	public PokemonController getAppController()
+	{
+		return appController;
+	}
 }
